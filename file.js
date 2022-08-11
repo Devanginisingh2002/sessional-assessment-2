d2fe95a91c378064c5057172f7a748cc797e0149
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2022-08-11 19:48:28 +0530

    demo

--- file.js
+++ file.js
@@ -1,36 +1,34 @@
-document.querySelector('.btn').addEventListener('submit',(e) => {
+document.querySelector('.btn').addEventListener('click',(e) => {
     e.preventDefault();
 
     const xhr = new XMLHttpRequest();
-    const searchValue = document.querySelector('#my-text').value
-    const url = `https://jsonplaceholder.typicode.com/posts?title=${dataSearch}`
+    const search_Value = document.querySelector('#search-text').value
+    const url = `https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09`
     xhr.open('GET',url, true);
+    let template = '';
 
-    const array_name = [item1, item2, item3];
 
-    xhr.onreadystatechange = () => {
-        if(xhr.status === 200 && xhr.readyState === 4) {
-            const response = JSON.parse(xhr.responseText)
-            //console.log(response)
+    xhr.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
 
-            let template = ''
-            for(let i=0; i < response.items.length; i++) {
+            const response = JSON.parse(xhr.responseText);
+            // console.log(response)
+
+            for(let i=0; i < response.length; i++) {
                 template += `
                     <div class="video-items" style="width: 20%; margin: 1.2em;">
-                        // <a href="https://www.youtube.com/watch?v=${response.items[i].id.videoId}" target="_blank">
-                        //     <img style="width: 100%" src="${response.items[i].snippet.thumbnails.high.url}" />
-                        // </a>
-                        <h5>${response.items[i].snippet.title}</h5>
-                        <p style="color: gray">${response.items[i].snippet.description}</p>
+
+                        <p style="color: gray">${response[i].userid}</p>
+                        <p style="color: gray">${response[i].id}</p>
+                        <p style="color: gray">${response[i].title}</p>
+                        <p style="color: gray">${response[i].body}</p>
                     </div>
                 `
             }
 
-            document.querySelector('#my-container').innerHTML = template
+            document.querySelector('#mycontainer').innerHTML = template 
         }
     }
 
     xhr.send()
 })
-
-
